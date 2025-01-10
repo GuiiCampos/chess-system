d6adeeea5d80910507c965f5be4746014b875e88
Guilherme Campos <guiicampos31@gmail.com>
2025-01-10 19:29:57 -0300

    Implementing possible moves of King

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -30,7 +30,16 @@ public class Rook extends ChessPiece {
         if (getBoard().positionExists(pos) && isThereOpponentPiece(pos)) {
             aux[pos.getRow()][pos.getColumn()] = true;
         }
+        //Below
+        pos.setValues(position.getRow() + 1, position.getColumn());
 
+        while (getBoard().positionExists(pos) && !getBoard().thereIsAPiece(pos)) {
+            aux[pos.getRow()][pos.getColumn()] = true;
+            pos.setRow(pos.getRow() + 1);
+        }
+        if (getBoard().positionExists(pos) && isThereOpponentPiece(pos)) {
+            aux[pos.getRow()][pos.getColumn()] = true;
+        }
         //Left
         pos.setValues(position.getRow(), position.getColumn() - 1);
 
@@ -41,24 +50,12 @@ public class Rook extends ChessPiece {
         if (getBoard().positionExists(pos) && isThereOpponentPiece(pos)) {
             aux[pos.getRow()][pos.getColumn()] = true;
         }
-
         //Right
         pos.setValues(position.getRow(), position.getColumn() + 1);
 
         while (getBoard().positionExists(pos) && !getBoard().thereIsAPiece(pos)) {
             aux[pos.getRow()][pos.getColumn()] = true;
             pos.setColumn(pos.getColumn() + 1);
-        }
-        if (getBoard().positionExists(pos) && isThereOpponentPiece(pos)) {
-            aux[pos.getRow()][pos.getColumn()] = true;
-        }
-
-        //Below
-        pos.setValues(position.getRow() + 1, position.getColumn());
-
-        while (getBoard().positionExists(pos) && !getBoard().thereIsAPiece(pos)) {
-            aux[pos.getRow()][pos.getColumn()] = true;
-            pos.setRow(pos.getRow() + 1);
         }
         if (getBoard().positionExists(pos) && isThereOpponentPiece(pos)) {
             aux[pos.getRow()][pos.getColumn()] = true;
