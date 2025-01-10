210cd8d7cfa054d6242036b9b270f167f47967c5
Guilherme Campos <guiicampos31@gmail.com>
2025-01-10 17:25:01 -0300

    Implementing possible moves of Rook

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -17,6 +18,51 @@ public class Rook extends ChessPiece {
     @Override
     public boolean[][] possibleMoves() {
         boolean[][] aux = new boolean[getBoard().getRows()][getBoard().getColumns()];
+        Position pos = new Position(0,0);
+
+        //Above
+        pos.setValues(position.getRow() - 1, position.getColumn());
+
+        while (getBoard().positionExists(pos) && !getBoard().thereIsAPiece(pos)) {
+            aux[pos.getRow()][pos.getColumn()] = true;
+            pos.setRow(pos.getRow() - 1);
+        }
+        if (getBoard().positionExists(pos) && isThereOpponentPiece(pos)) {
+            aux[pos.getRow()][pos.getColumn()] = true;
+        }
+
+        //Left
+        pos.setValues(position.getRow(), position.getColumn() - 1);
+
+        while (getBoard().positionExists(pos) && !getBoard().thereIsAPiece(pos)) {
+            aux[pos.getRow()][pos.getColumn()] = true;
+            pos.setColumn(pos.getColumn() - 1);
+        }
+        if (getBoard().positionExists(pos) && isThereOpponentPiece(pos)) {
+            aux[pos.getRow()][pos.getColumn()] = true;
+        }
+
+        //Right
+        pos.setValues(position.getRow(), position.getColumn() + 1);
+
+        while (getBoard().positionExists(pos) && !getBoard().thereIsAPiece(pos)) {
+            aux[pos.getRow()][pos.getColumn()] = true;
+            pos.setColumn(pos.getColumn() + 1);
+        }
+        if (getBoard().positionExists(pos) && isThereOpponentPiece(pos)) {
+            aux[pos.getRow()][pos.getColumn()] = true;
+        }
+
+        //Below
+        pos.setValues(position.getRow() + 1, position.getColumn());
+
+        while (getBoard().positionExists(pos) && !getBoard().thereIsAPiece(pos)) {
+            aux[pos.getRow()][pos.getColumn()] = true;
+            pos.setRow(pos.getRow() + 1);
+        }
+        if (getBoard().positionExists(pos) && isThereOpponentPiece(pos)) {
+            aux[pos.getRow()][pos.getColumn()] = true;
+        }
 
         return aux;
     }
